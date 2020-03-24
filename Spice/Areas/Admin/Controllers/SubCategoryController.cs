673bbb770d777d978b48594a17c7cd453bcd9c82
longnc <nclong92@gmail.com>
2020-03-24 10:58:56 +0700

    SubCategory Create Get Action

--- Spice/Areas/Admin/Controllers/SubCategoryController.cs
+++ Spice/Areas/Admin/Controllers/SubCategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,22 @@ namespace Spice.Areas.Admin.Controllers
                                         .ToListAsync();
 
             return View(subCategories);
+        }
+
+        // Get Create
+        public async Task<IActionResult> Create()
+        {
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = new Models.SubCategory(),
+                SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name)
+                                            .Select(p => p.Name)
+                                            .Distinct()
+                                            .ToListAsync()
+            };
+
+            return View(model);
         }
     }
 }
