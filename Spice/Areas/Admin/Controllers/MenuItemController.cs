5c775d1c7fa1593aeec4e7359a38fe070bc18922
nclong92 <nclong92@gmail.com>
2020-04-02 17:19:22 +0700

    Menu Item - Index

---
+++ Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public MenuItemController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
+        {
+            _db = db;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync();
+
+            return View(model);
+        }
+    }
+}
