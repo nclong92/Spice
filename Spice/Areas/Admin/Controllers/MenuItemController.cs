338b76a8c33ca30fbe734fe2e4535f1bbd90b8c7
nclong92 <nclong92@gmail.com>
2020-04-04 11:59:29 +0700

    textarea with tiny cloud

--- Spice/Areas/Admin/Controllers/MenuItemController.cs
+++ Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,10 +18,18 @@ namespace Spice.Areas.Admin.Controllers
         private readonly ApplicationDbContext _db;
         private readonly IWebHostEnvironment _webHostEnvironment;
 
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
+
         public MenuItemController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
             _webHostEnvironment = webHostEnvironment;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = _db.Category,
+                MenuItem = new Models.MenuItem()
+            };
         }
 
         public async Task<IActionResult> Index()
@@ -28,6 +37,11 @@ namespace Spice.Areas.Admin.Controllers
             var model = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync();
 
             return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
         }
     }
 }
