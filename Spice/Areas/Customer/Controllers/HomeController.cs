11b5c6896f5437ddb2f3b2e5b5702ba9fd777e7e
nclong92 <nclong92@gmail.com>
2020-04-05 10:31:55 +0700

    Landing Page - Home Index Action

--- Spice/Areas/Customer/Controllers/HomeController.cs
+++ Spice/Areas/Customer/Controllers/HomeController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Spice.Data;
+using Spice.Models.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,15 +16,27 @@ namespace Spice.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var IndexVM = new IndexViewModel()
+            {
+                MenuItem = await _db.MenuItem
+                                    .Include(m => m.Category)
+                                    .Include(m => m.SubCategory)
+                                    .ToListAsync(),
+                Category = await _db.Category.ToListAsync(),
+                Coupon = await _db.Coupon.Where(m => m.IsActive == true).ToListAsync()
+            };
+
+            return View(IndexVM);
         }
     }
 }
