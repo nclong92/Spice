8b10be43564cd11f3e1d95ed6e558bd6baf3e6c5
longnc <nclong92@gmail.com>
2020-12-23 11:09:17 +0700

    remove coupon

--- Spice/Areas/Customer/Controllers/CartController.cs
+++ Spice/Areas/Customer/Controllers/CartController.cs
@@ -80,5 +80,12 @@ namespace Spice.Areas.Customer.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult RemoveCoupon()
+        {
+            HttpContext.Session.SetString(SD.ssCouponCode, string.Empty);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
