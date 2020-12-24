2027a40c4169c34aceafceec30edd94603cd739d
longnc <nclong92@gmail.com>
2020-12-24 17:08:08 +0700

    Order History

--- Spice/Areas/Customer/Controllers/OrderController.cs
+++ Spice/Areas/Customer/Controllers/OrderController.cs
@@ -36,6 +36,43 @@ namespace Spice.Areas.Customer.Controllers
             return View(orderDetailsViewModel);
         }
 
+        [Authorize]
+        public async Task<IActionResult> OrderHistory()
+        {
+            var claimsIdentitty = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentitty.FindFirst(ClaimTypes.NameIdentifier);
+
+            var orderList = new List<OrderDetailsViewModel>();
+            var OrderHeaderList = await _db.OrderHeader.Include(o => o.ApplicationUser)
+                                            .Where(u => u.UserId == claim.Value).ToListAsync();
+
+            foreach (var item in OrderHeaderList)
+            {
+                var individual = new OrderDetailsViewModel
+                {
+                    OrderHeader = item,
+                    OrderDetails = await _db.OrderDetails.Where(o => o.OrderId == item.Id).ToListAsync()
+                };
+
+                orderList.Add(individual);
+            }
+
+            return View(orderList);
+        }
+
+        public async Task<IActionResult> GetOrderDetails(int Id)
+        {
+            var orderDetailsViewModel = new OrderDetailsViewModel()
+            {
+                OrderHeader = await _db.OrderHeader.FirstOrDefaultAsync(m => m.Id == Id),
+                OrderDetails = await _db.OrderDetails.Where(m => m.OrderId == Id).ToListAsync()
+            };
+
+            orderDetailsViewModel.OrderHeader.ApplicationUser = await _db.ApplicationUser.FirstOrDefaultAsync(u => u.Id == orderDetailsViewModel.OrderHeader.UserId);
+
+            return PartialView("_IndividualOrderDetails", orderDetailsViewModel);
+        }
+
         public IActionResult Index()
         {
             return View();
